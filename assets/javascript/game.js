46143d4ab3e10ae00cb108ce8a2d0c4fb89d39c6
Daniel Vadil <dvadz@yahoo.com>
2019-02-12 17:44:22 -0500

    linked the js file and initiated an array of words and random number generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var words = [
+    'perspective',
+    'exellecent',
+    'prerogative',
+    'microwave',
+    'transplant',
+    'witchcraft',
+    'syndrome',
+    'twelfth',
+    'oxygen',
+    'quartz'
+];
+
+var randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber);
