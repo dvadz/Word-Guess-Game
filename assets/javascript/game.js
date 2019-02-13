60b682646ba48558ad355018bb4c5b28f43859fe
Daniel Vadil <dvadz@yahoo.com>
2019-02-13 18:02:15 -0500

    changed 'secretWordDisplay' to array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
 "use scrict"
 
-var randonNumber, secretWord, secretWordDisplay;
+var randonNumber, 
+    secretWord, 
+    secretWordDisplay,
+    gameOver = false, 
+    attemptsLeft = 10;
 
 var alphabet = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
@@ -20,13 +24,21 @@ var listOfWords = [
 //pick a random word from the array
 randomNumber = Math.floor(Math.random() * 10);
 secretWord = listOfWords[randomNumber];
+secretWord = secretWord.toUpperCase();
+console.log(secretWord);
 
 //display "secret word"
-secretWordDisplay = "";
+secretWordDisplay = [];
 for (var i = 0; i < secretWord.length; i++) {
-    secretWordDisplay += "_"
+    secretWordDisplay.push("_");
 }
-$("#h1_the_secret_word").text(secretWordDisplay);
+$("#h1_the_secret_word").text(secretWordDisplay.join(""));
+
+// --------------------------------------------------------------------------------
+// ------------ INITIALIZATION OF THE GAME SHOULD END HERE ------------------------
+
+
+
 
 $("#div_alphabet").on("click", function (event) {
 
@@ -40,16 +52,12 @@ $("#div_alphabet").on("click", function (event) {
 
     //get the identity of the letter that was clicked
     var letterThatWasClicked = event.target.innerText;
-    console.log(`var letterThatWasClicked: ${letterThatWasClicked}`);
 
     //remove that letter from the alphabet array
     for (var i = 0; i < alphabet.length; i++) {
         if (alphabet[i] === letterThatWasClicked) {
             alphabet.splice(i, 1);
-            console.log(`matched: ${i}`);
         }
     }
-    console.log(alphabet);
-
 
 });
